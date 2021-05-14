27045a85b7ca638985eb527fc02ef49ae7493259
markel <markelarauzo7@gmail.com>
2021-05-14 12:46:16 +0200

    Add web profiler to dev dependencies

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -8,4 +8,5 @@ return [
     SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
